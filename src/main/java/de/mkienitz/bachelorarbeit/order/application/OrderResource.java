5427dc9a5d320b421f1f10f7b4853aa89c3c9d9e
JapuDCret <JapuDCret@users.noreply.github.com>
2021-01-12 16:12:12 +0100

    remove unnecessary import and injection

--- src/main/java/de/mkienitz/bachelorarbeit/order/application/OrderResource.java
+++ src/main/java/de/mkienitz/bachelorarbeit/order/application/OrderResource.java
@@ -14,12 +14,12 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ContextResolver;
-import javax.ws.rs.ext.Providers;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -36,9 +36,6 @@ public class OrderResource {
 
     @Inject
     private OrderService service;
-
-    @Context
-    private Providers providers;
 
     private CartServiceClient cartServiceClient;
 
